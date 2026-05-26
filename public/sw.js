e6b12539a3072da0d737d7bb9d637fba0be0ea7f
Mambaul Ulum <mambaululum00@gmail.com>
2026-05-26 16:16:12 +0700

    feat: tambah service worker untuk PWA install

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'mambaul-ulum-v1';
+const urlsToCache = ['/'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
